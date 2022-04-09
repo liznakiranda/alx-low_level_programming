0a843c254da4d0e99a8b91d02cfbab6ba8ad9876
liznakiranda <lizanakiranda@gmail.com>
2022-04-09 10:28:52 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
